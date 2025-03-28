a6feca80d348d697d93589e3b6c838086779f430
Akash Kumar Prasad <140340713+Akash8585@users.noreply.github.com>
2025-03-28 10:11:13 +0530

    Repo Summary created App structure created

---
+++ src/lib/github-loader.ts
@@ -0,0 +1,30 @@
+import { GithubRepoLoader} from '@langchain/community/document_loaders/web/github'
+import { Document } from '@langchain/core/documents'
+import { summariseCode } from './gemini'
+
+
+export const loadGithubRepo = async (githubUrl: string, githubToken?: string) => {
+    const loader = new GithubRepoLoader(githubUrl, {
+        accessToken: githubToken,
+        branch: 'main',
+        ignoreFiles:['package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lock'],
+        recursive: true,
+        unknown: 'warn',
+        maxConcurrency: 5,
+    })
+    const docs = await loader.load()
+    return docs
+}
+
+export const indexGithubRepo = async (projectId: string, githubUrl: string, githubToken?:string) =>{
+    const docs = await loadGithubRepo(githubUrl, githubToken)
+    const allEmbeddings = await generateEmbeddings(docs)
+}
+
+
+const generateEmbeddings = async (docs: Document[]) => {
+    return await Promise.all(docs.map(async doc => {
+        const summary = await summariseCode(doc)
+    }))
+
+}
